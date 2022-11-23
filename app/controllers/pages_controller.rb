780765fc761e9d5d230f3c64a5558326bec978cb
Alex Drew <alexander.william.drew@gmail.com>
2022-11-23 16:23:04 +0800

    added views for devise

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,14 +1,15 @@
 class PagesController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:home]
   def home
   end
 
 
   def julia
-   
+
   end
 
   def components_alex
-    # @grandma = Grandma.new
+
   end
 
   def alex
