76b77c7351855626cf28b165853ee016e5f5171b
Alexander Neukomm <alexander.neukomm@gmail.com>
2022-11-24 08:02:58 +0100

    create listing works

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -12,4 +12,26 @@ class ListingsController < ApplicationController
     @listing = Listing.find(params[:id])
   end
 
+  def new
+    @listing = Listing.new
+  end
+
+  def create
+    @user = current_user
+    @listing = Listing.new(listing_params)
+    @listing.user = @user
+
+    if @listing.save
+      redirect_to listing_path(@listing)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def listing_params
+    params.require(:listing).permit(:price, :bedrooms, :bathrooms, :address, :description, :property_type, :area_size, :floor, :garden, :balcony, :parking, :family_status, :occupation, :pets, :lift, :furnished, :user_id)
+  end
+
 end
