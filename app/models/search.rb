3a61787e9dc4715a3a83e364970fd602f847f332
Joergen Tjoennteig <jorgenlt@outlook.com>
2023-02-19 21:58:15 +0700

    cleaning up models

--- app/models/search.rb
+++ app/models/search.rb
@@ -1,6 +1,4 @@
 class Search < ApplicationRecord
   belongs_to :user
   has_many :matches
-  # validates :price, :bedrooms, :bathrooms, :address, :photos, :property_type, :area_size, :floor, presence: true
-
 end
